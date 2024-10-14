769af03f19a34bf8ccd3c7a343667d2ebc0796b9
Mahmoud031 <mahmoudrady007@gmail.com>
2024-10-14 22:05:42 +0300

    create Notes List view

---
+++ lib/views/notes_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/notes_view_body.dart';
+
+class NotesView extends StatelessWidget {
+  const NotesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+      body: NotesViewBody(),
+    );
+  }
+}
