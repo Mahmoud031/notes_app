38830199caccb969a4dbd376282d8e25b1070531
Mahmoud031 <mahmoudrady007@gmail.com>
2024-10-17 01:02:17 +0300

    add border radius to the bottom sheet

--- lib/widgets/notes_list_view.dart
+++ lib/widgets/notes_list_view.dart
@@ -5,11 +5,16 @@ class NotesListView extends StatelessWidget {
   const NotesListView({super.key});
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(itemBuilder: (context ,index){
-      return Padding(
-        padding: const EdgeInsets.only(top:8.0),
-        child: const NotesItem(),
-      );
-    });
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      child: ListView.builder(
+        padding: EdgeInsets.zero,
+        itemBuilder: (context ,index){
+        return Padding(
+          padding: const EdgeInsets.only(top:8.0),
+          child: const NotesItem(),
+        );
+      }),
+    );
   }
 }
