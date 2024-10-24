bdfaab39ef93ed47590bf8eafbccf6621db5552f
Mahmoud031 <mahmoudrady007@gmail.com>
2024-10-24 15:53:42 +0300

    register adapter

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes_app/constants.dart';
+import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/notes_view.dart';
 
-void main() async{
+void main() async {
   await Hive.initFlutter(); // initialize hive flutter
   await Hive.openBox(kNotesBox); //  box to store
+  Hive.registerAdapter(
+      NoteModelAdapter()); // register to treate or store note model(in type adapter)
   runApp(const NotesApp());
-
 }
 
 class NotesApp extends StatelessWidget {
@@ -16,12 +18,8 @@ class NotesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        fontFamily: 'Poppins'
-      ),
-      home: const NotesView()
-    );
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
+        home: const NotesView());
   }
 }
